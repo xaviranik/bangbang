7b21582cf4d6724ee235e157bbfd6a54563b9d3f
xaviranik <xaviranik.me@gmail.com>
2019-12-16 10:33:15 +0600

    Added: Controllers, routes, views

---
+++ controllers/index.php
@@ -0,0 +1,6 @@
+<?php
+require 'views/index.bang.php';
+
+
+//Getting all records from todos table
+$task = $query->selectAll('todos');
--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 
 $query = require 'core/bootstrap.php';
 
-require 'views/index.bang.php';
-
-
-
-//Getting all records from todos table
-$task = $query->selectAll('todos');
+$route = [
+    '' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+];
