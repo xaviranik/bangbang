5af8f95a6b0a470af509841235a7181f3e879977
xaviranik <xaviranik.me@gmail.com>
2019-12-18 22:43:32 +0600

    Added: Dynamic PDO inserts on queryBuilder

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -16,4 +16,23 @@ class QueryBuilder {
         return $statement->fetchAll(PDO::FETCH_CLASS);
         
     }
+    
+    public function insert($table, $parameters)
+    {
+        $sql = sprintf(
+            'insert into %s (%s) values (%s)',
+            $table,
+            implode(', ', array_keys($parameters)),
+            ':' . implode(', :', array_keys($parameters))
+        );
+
+        try {
+            $statement = $this->pdo->prepare($sql);
+            $statement->execute($parameters);
+
+        } catch(PDOException $e) {
+            die('Whoops, something went wrong: ' . $e);
+        }
+
+    }
 }
--- index.php
+++ index.php
@@ -3,3 +3,8 @@
 require 'core/bootstrap.php';
 
 require Router::load('routes.php')->direct(Request::uri(), Request::method());
+
+$app['database']->insert('todos', [
+    'todo' => 'foobar',
+    'completed' => 0
+]);
